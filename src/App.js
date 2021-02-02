8f84d96bf07a7661aca93629e8f62c57b63b5b36
SHASHI JAISWAL <shashi.jaiswal17@gmail.com>
2021-02-02 18:28:51 +0530

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -1,143 +1,26 @@
-import React from "react";
-import HomePage from "./HomePage";
-import { BrowserRouter as Router, Route, Link, Switch } from "react-router-dom";
-import CheckoutStep from "./CheckoutSteps";
-import VRanDetailPageComponent from "./VRanDetailPageComponent";
-import Office365CartDetailComponent from "./Office365CartDetailComponent";
-import CartDetailsComponent from "./CartDetailsComponent";
-import QuoteComponent from "./QuoteComponent";
-import Office365DetailComponent from "./Office365DetailComponent";
-import MyAccountDetailPageComponent from "./MyAccounDetailPageComponent";
-import SubmitQuoteComponent from "./SubmitQuoteComponent";
-import EmptyCartComponent from "./EmptyCartComponent";
-import Office365ImageComponent from "./Office365ImageComponent";
-import PrivateCloudServiceComponent from "./PrivateCloudServiceComponent";
-import BrandDetailComponent from "./BrandDetailComponent";
-import CatlogDetailsComponent from "./CatlogDetailsComponent";
-import LoginComponent from "./LoginComponent";
-import NgInxPlusComponent from "./NgInxPlusComponent";
-import CartnginxComponent from "./CartnginxComponent";
-import CartMongodbComponent from "./CartMongodbComponent";
-import EmptyCartDetailComponent from "./EmptyCartDetailComponent";
-import NginxCartdetailComponent from "./NginxCartdetailComponent";
-import { connect } from "react-redux";
-import { bindActionCreators } from "redux";
-import * as Actions from "./action";
-import { Provider } from "react-redux";
-import {   createStore } from "redux";
-import AddAllReducers from "./reducers";
-import { BrowserRouter  } from "react-router-dom";
-import NavBar from "./NavBar";
-const mapStateToProps = (state) => ({
-  sendProductToCart: state.sendProductToCart,
-});
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
 
-const mapDispatchToProps = (dispatch) => ({
-  actions: bindActionCreators(Actions, dispatch),
-});
-const store = createStore(
-  AddAllReducers,
-  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
-);
 function App() {
   return (
-    <Provider store={store}>
- <BrowserRouter>
- 
-
- 
-    <div>
-
-      <Router>
-        {/* <NavBar/> */}
-        <Switch>
-          <Route exact path="/" component={HomePage}></Route>
-          <Route
-            exact
-            path="/product-detail"
-            component={VRanDetailPageComponent}
-          ></Route>
-          <Route exact path="/checkout" component={CheckoutStep}></Route>
-          <Route
-            exact
-            path="/office365EnterpriceDetails"
-            component={Office365CartDetailComponent}
-          ></Route>
-          <Route
-            exact
-            path="/cartDetail"
-            component={CartDetailsComponent}
-          ></Route>
-          <Route exact path="/quoteDetail" component={QuoteComponent}></Route>
-          <Route exact path="/new" component={Office365DetailComponent}></Route>
-          <Route
-            exact
-            path="/myaccountdetailPage"
-            component={MyAccountDetailPageComponent}
-          ></Route>
-          <Route
-            exact
-            path="/submitquote"
-            component={SubmitQuoteComponent}
-          ></Route>
-          <Route exact path="/emptycart" component={EmptyCartComponent}></Route>
-          <Route
-            exact
-            path="/privatecloudservicedetail"
-            component={PrivateCloudServiceComponent}
-          ></Route>
-          <Route
-            exact
-            path="/brandetail"
-            component={BrandDetailComponent}
-          ></Route>
-          <Route
-            exact
-            path="/catlogDetails"
-            component={CatlogDetailsComponent}
-          ></Route>
-          <Route exact path="/login" component={LoginComponent}></Route>
-          <Route
-            exact
-            path="/nginxplusdetails"
-            component={NgInxPlusComponent}
-          ></Route>
-          <Route
-            exact
-            path="/cartnginxdetail"
-            component={CartnginxComponent}
-          ></Route>
-          <Route
-            exact
-            path="/mongocartdetail"
-            component={CartMongodbComponent}
-          ></Route>
-          <Route
-            exact
-            path="/emptyCartDetailComponent"
-            component={EmptyCartDetailComponent}
-          ></Route>
-          <Route
-            exact
-            path="/nginxCartdetailComponent"
-            component={NginxCartdetailComponent}
-          ></Route>
-          <Route
-            exact
-            path="/office365_details"
-            component={Office365DetailComponent}
-          ></Route>
-          <Route
-            exact
-            path="/office365_cartdetails"
-            component={Office365CartDetailComponent}
-          ></Route>
-        </Switch>
-      </Router>
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
     </div>
-    </BrowserRouter>
-    </Provider>
   );
 }
 
-export default connect(mapStateToProps, mapDispatchToProps)(App);
+export default App;
--- src/index.js
+++ src/index.js
@@ -1,35 +1,16 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import "./index.css";
-import App from "./App";
-import * as serviceWorker from "./serviceWorker";
-import { createStore, combineReducers } from "redux";
-import AddAllReducers from "./reducers";
-import { Provider } from "react-redux";
-import { CookiesProvider } from 'react-cookie';
-
-const store = createStore(
-  AddAllReducers,
-   
-+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
- );
-
-
-//const state = store.getState();
-console.clear();
-console.log(store.getState())
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
   <React.StrictMode>
-    <Provider store={store}>,
-    <CookiesProvider>
-      <App />
-      </CookiesProvider>
-    </Provider>,
+    <App />
   </React.StrictMode>,
-
-  document.getElementById("root")
+  document.getElementById('root')
 );
+
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
